2bb92a266e710ae2e33f50ab6b3f5a7b54e18fac
haltse <haltse@gmail.com>
2018-12-05 16:26:17 -0700

    Playing around: added the DB format

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ def header():
     print("*****" * 10)
 
 
+moviedb = [{'Movie Name': 'Bladerunner', 'release year': 1984, 'Director': 'Ridely Scott'},
+           {'Movie Name': 'Star Wars', 'release year': 1977, 'Director': 'George Lucas'}]
+
+
 def add_movie():
+    input("We're adding a movie,  Let's start with the name")
     pass
+
 
 def list_movies():
     pass
@@ -15,17 +21,14 @@ def search_movies():
     pass
 
 
-
-
 header()
 action = ''
 while action.lower() != 'x':
-    action = input("Would you like to [a]dd a movie,[l]ist all movies in the library,[s]earch for movies or E[x]it the app?")
+    action = input(
+        "Would you like to [a]dd a movie,[l]ist all movies in the library,[s]earch for movies or E[x]it the app?")
     if action.lower() == 'a':
         pass
     elif action.lower() == 'l':
         pass
     elif action.lower() == 's':
         pass
-
-
