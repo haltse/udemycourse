53e4c11fb9201279e960ff0b6ac684f04fe238ac
haltse <haltse@gmail.com>
2018-12-05 08:58:39 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+def header():
+    print("*****" * 10)
+    print("*****************  The movie place ***************")
+    print("*****" * 10)
+
+
+def add_movie():
+    pass
+
+def list_movies():
+    pass
+
+
+def search_movies():
+    pass
+
+
+
+
+header()
+action = ''
+while action.lower() != 'x':
+    action = input("Would you like to [a]dd a movie,[l]ist all movies in the library,[s]earch for movies or E[x]it the app?")
+    if action.lower() == 'a':
+        pass
+    elif action.lower() == 'l':
+        pass
+    elif action.lower() == 's':
+        pass
+
+
