e8c0bc9bb5a4cd412c223d84d46361a425013e88
haltse <haltse@gmail.com>
2018-12-07 16:17:36 -0700

    update  for home

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ def header():
     print("*****" * 10)
 
 
-moviedb = [{'Movie Name': 'Bladerunner', 'release year': 1984, 'Director': 'Ridely Scott'},
+moviedb = [{'Movie Name': "", 'release year': "", 'Director': ""},
+           {'Movie Name': 'Bladerunner', 'release year': 1984, 'Director': 'Ridely Scott'},
            {'Movie Name': 'Star Wars', 'release year': 1977, 'Director': 'George Lucas'}]
 
 
 def add_movie():
-    input("We're adding a movie,  Let's start with the name")
-    pass
+    name = input("We're adding a movie,  Let's start with the name")
+    year = input("which year was it released?")
+    direct = input("And who directed it?")
+    moviedb.append({'Movie Name': name, 'release year': year, 'Director': direct})
 
 
 def list_movies():
-    pass
+    # allmovies =[items for items in moviedb]
+    for k in range(0, len(moviedb)):
+        print(item for item in moviedb[k].items())
+
 
 
 def search_movies():
@@ -27,8 +33,8 @@ while action.lower() != 'x':
     action = input(
         "Would you like to [a]dd a movie,[l]ist all movies in the library,[s]earch for movies or E[x]it the app?")
     if action.lower() == 'a':
-        pass
+        add_movie()
     elif action.lower() == 'l':
-        pass
+        list_movies()
     elif action.lower() == 's':
         pass
