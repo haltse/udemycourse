d68e79cb104e883c8a2800c2b74a8bf4c4a36dbb
haltse <haltse@gmail.com>
2018-12-13 15:31:38 -0700

    update  for home

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ def header():
     print("*****" * 10)
 
 
-moviedb = [{'Movie Name': "", 'release year': "", 'Director': ""},
+moviedb = [{'Movie Name': "Movie Name", 'release year': "release year", 'Director': "Director"},
            {'Movie Name': 'Bladerunner', 'release year': 1984, 'Director': 'Ridely Scott'},
+           {'Movie Name': 'Ghostbusters', 'release year': 1984, 'Director': 'Ivan Reitman'},
            {'Movie Name': 'Star Wars', 'release year': 1977, 'Director': 'George Lucas'}]
 
 
@@ -16,15 +17,41 @@ def add_movie():
     moviedb.append({'Movie Name': name, 'release year': year, 'Director': direct})
 
 
-def list_movies():
+def list_movies(woot):
     # allmovies =[items for items in moviedb]
-    for k in range(0, len(moviedb)):
-        print(item for item in moviedb[k].items())
-
+    print(str({woot[0]['Movie Name']}), {woot[0]['release year']}, {woot[0]['Director']})
+    for k in range(1, len(woot)):
+        # print(woot[k].values())
+        print(str({woot[k]['Movie Name']}), {woot[k]['release year']}, {woot[k]['Director']})
 
 
 def search_movies():
+    action = input("Would you like to find a movie by [t]itle, [y]ear released or by [d]irector?")
+    if action.lower() == 't':
+        search = input('What is the title?')
+        for k in range(1, len(moviedb)):
+            if moviedb[k]['Movie Name'] == str(search):
+                print(
+                    f" the title is {moviedb[k]['Movie Name']} which was released in {moviedb[k]['release year']} and was directed by {moviedb[k]['Director']}")
+                continue
+            else:
+                print("I'm sorry I can't find that movie")
+    elif action.lower() == 'y':
+        search = input('What year was it released ?')
+        for k in range(1, len(moviedb)):
+            if moviedb[k]['release year'] == search.strip():
+                print(
+                    f" the title is {moviedb[k]['Movie Name']} which was released in {moviedb[k]['release year']} and was directed by {moviedb[k]['Director']}")
+                continue
+            else:
+                print("I'm sorry I can't find that movie")
+        pass
+    elif action.lower() == 'd':
+        pass
+
+def return_matches(search_term,search_type):
     pass
+
 
 
 header()
@@ -35,6 +62,6 @@ while action.lower() != 'x':
     if action.lower() == 'a':
         add_movie()
     elif action.lower() == 'l':
-        list_movies()
+        list_movies(moviedb)
     elif action.lower() == 's':
-        pass
+        search_movies()
